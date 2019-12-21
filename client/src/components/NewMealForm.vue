f365ac6b5a59f0ac7cc84d4576b261463c8223fc
Iain <iain2eyes@gmail.com>
2019-12-21 17:39:41 +0000

    Add ability to save ingredient and value together

--- client/src/components/NewMealForm.vue
+++ client/src/components/NewMealForm.vue
@@ -2,7 +2,16 @@
   <div class="form">
     <form class="" v-on:submit.prevent="handleSubmit();" action="index.html" method="post">
       <input type="text" name="" value="" placeholder="Name" v-model="mealName">
-      <input type="text" name="" value="" placeholder="Ingredients" v-model="ingredients">
+      <!-- <input type="text" name="" value="" placeholder="Ingredient" v-model="ingredients"> -->
+      <div v-for="(ingredient, index) in this.ingredients">
+      <input type="text" v-model="ingredient.ingredient">
+      </div>
+      <div v-for="(ingredient, index) in this.quantities">
+      <input type="text" v-model="ingredient.quantity">
+      </div>
+      <h4 @click="addIngredient">
+        New Ingredient
+      </h4>
       <input type="text" name="" value="" placeholder="Instructions" v-model="instructions">
       <input type="text" name="" value="" placeholder="URL" v-model="image">
       <input type="submit" name="" value="Accept Recipe">
@@ -20,7 +29,8 @@ export default {
   data(){
     return {
       mealName: "",
-      ingredients: "",
+      ingredients: [],
+      quantities: [],
       instructions: "",
       image: "",
       mealObject: {}
@@ -29,7 +39,7 @@ export default {
   methods: {
     handleSubmit() {
       this.mealObject.name = this.mealName;
-      this.mealObject.ingredients = this.ingredients;
+      this.combineIngredientQuantity();
       this.mealObject.instructions = this.instructions;
       this.mealObject.image = this.image;
       eventBus.$emit('new-meal', this.mealObject)
@@ -37,9 +47,20 @@ export default {
     },
     handleCloseForm(){
       eventBus.$emit('close-form')
-    }
-    }
+    },
+    addIngredient() {
+      this.ingredients.push({ ingredient: '' });
+      this.quantities.push({ quantity: '' });
+    },
+    combineIngredientQuantity(){
+      for(var i = 0; i < this.ingredients.length; ++i){
+        for(var ingredient in this.ingredients){
+          this.mealObject.ingredients = {[this.ingredients[i].ingredient]: parseInt(this.quantities[i].quantity)}
+          }
+        }
+      }
   }
+}
 
 </script>
 
@@ -51,6 +72,9 @@ export default {
     top:5%;
     left: 5%;
     background-color: white;
+  }
+  input{
+    border: 1px solid black;
   }
 
 </style>
--- client/src/components/ShoppingList.vue
+++ client/src/components/ShoppingList.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="shopping-list">
     <div>
-      <p v-for="(value, ingredient) of noDuplicateIngredients">{{ingredient}} : {{value}}</p>
+      <p v-for="meal in weekObject.week_one">{{meal.ingredients}}</p>
     </div>
     <button type="button" name="button" v-on:click="handleCloseShoppingList();">Close Shopping List</button>
 
@@ -12,7 +12,7 @@
 import {eventBus} from '../main.js'
 export default {
   name: "shopping-list",
-  props: ['noDuplicateIngredients'],
+  props: ['weekObject'],
   methods: {
     handleCloseShoppingList(){
       eventBus.$emit('close-shopping-list')
@@ -27,8 +27,9 @@ export default {
     position: absolute;
     height: 90vh;
     width: 90vw;
-    top: 5%;
-    left: 5%;
-    background-color: white;
+    top: 0;
+    left: 0;
+    background-color: lightgrey;
+    opacity: 0.9;
   }
 </style>
